c087de65a036bc84e110cd095def2b9ab458770d
JSupot <huchisupot@163.com>
2019-12-01 10:26:14 +0800

    feat: 替换webpack hook

--- index.js
+++ index.js
@@ -3,16 +3,6 @@ const fs = require('fs');
 const { getThemeFileList } = require('./lib/utils');
 const { ASTParser } = require('./lib/parser');
 
-// function test() {
-//   const ROOT_PATH = path.resolve(process.cwd());
-//   const FORM_RENDER_ANTD_PATH = path.resolve(ROOT_PATH, './node_modules/form-render/lib/widgets/antd');
-//   const fileList = getThemeFileList(FORM_RENDER_ANTD_PATH);
-
-//   ASTParser(fileList)
-// }
-
-// test()
-
 class FormRenderThemePlugin {
   constructor(options) {
     this.options = options;
@@ -20,12 +10,13 @@ class FormRenderThemePlugin {
 
   /* eslint-disable-next-line */
   apply(compiler) {
-    compiler.hooks.beforeRun.tap('FormRenderThemePlugin', (compiler) => {
+    compiler.hooks.afterEnvironment.tap('FormRenderThemePlugin', () => {
       const ROOT_PATH = path.resolve(process.cwd());
       const FORM_RENDER_ANTD_PATH = path.resolve(ROOT_PATH, './node_modules/form-render/lib/widgets/antd');
       const fileList = getThemeFileList(FORM_RENDER_ANTD_PATH);
 
-      ASTParser(fileList)
+      ASTParser(fileList);
+      // callback()
     });
   }
 }
